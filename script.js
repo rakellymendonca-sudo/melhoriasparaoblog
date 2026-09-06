8c0d56508dbd836dcc7c5b51b050f6ae9d1e403b
rakellymendonca-sudo <rakelly.mendonca@escola.pr.gov.br>
2026-09-06 11:24:15 -0300

    corpoPagina1

--- script.js
+++ script.js
@@ -21,5 +21,5 @@ const btnTemaEscuro = document.querySelector("btn-tema-escuro");
 btnTemaEscuro.addEventListener("click", mudaTema);
 
 function mudaTema(){
-  
+  const.corpoPagina = document.body;
 }
