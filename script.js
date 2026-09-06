bf28970ab4fda4878282ffc6b1aa05edb069f4d4
rakellymendonca-sudo <rakelly.mendonca@escola.pr.gov.br>
2026-09-06 11:26:55 -0300

    corpoPagina3

--- script.js
+++ script.js
@@ -24,5 +24,7 @@ function mudaTema(){
   const.corpoPagina = document.body;
   if (corpoPagina.classList.contains("tema-escuro")){
     corpoPagina.classList.remove("tema-escuro");
-  } 
+  } else {
+    corpoPagina.classList.add("tema-escuro");
+  }
 }
